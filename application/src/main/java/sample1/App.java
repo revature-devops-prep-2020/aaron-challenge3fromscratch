0fe438c180e4e7c3580fb654dc852a3bb5c0f260
AaronDownward <downward150@gmail.com>
2020-11-09 12:20:48 -0600

    adding sample app

---
+++ application/src/main/java/sample1/App.java
@@ -0,0 +1,32 @@
+package sample1;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        Tomcat server = new Tomcat();
+        server.setPort(8080);
+        server.setBaseDir(new File("target/tomcat").getAbsolutePath());
+        server.getConnector();
+        server.addWebapp("", new File("src/main/static").getAbsolutePath());
+        try {
+            server.start();
+            server.getServer().await();
+        } catch (LifecycleException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println();
+        }
+    }
+}
+
